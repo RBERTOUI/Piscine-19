41d8b459d52a4daf4e439093a9ab0e752b335c8b
Tialys12 <Tialys12@github.com>
2020-02-11 01:36:40 +0100

    C03

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,35 @@
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while(((s1[i] != '\0') && (s2[i] != '\0') && (i < n)))
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    if (i == n)
+        return (0);
+    return (s1[i] - s2[i]);
+}
+
+char *ft_strstr(char *str, char *to_find)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    if(to_find[j] == '\0')
+        return (str);
+    while (to_find[j] != '\0')
+        j++;
+    while (str[i] != '\0')
+    {
+        if ( ft_strncmp(&str[i], to_find, j) == 0)
+            return (&str[i]);
+        i++;
+    }
+    return (NULL);
+}
