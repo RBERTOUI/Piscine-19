9b8b47cf80f7ad327aa5bb3ede6dc2b432be9fbb
Romain Bertouille <rbertoui@w-r8-p4.s19.be>
2020-02-12 21:55:17 +0100

    C04 et C05

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbertoui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/12 20:13:11 by rbertoui          #+#    #+#             */
+/*   Updated: 2020/02/12 21:53:08 by rbertoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	int nbr;
+	
+	nbr = nb;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	return (nbr * ft_recursive_factorial(nb - 1));
+}
